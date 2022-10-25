264fca6bef2be58140f69af6ecfe02775c840a3b
Rhea Abayan <rhenayanz@gmail.com>
2022-10-25 10:21:46 +0800

    fetch api using rtk query based on #12

--- src/App.js
+++ src/App.js
@@ -6,25 +6,32 @@ import GlobalStyle from './styles/GlobalStyles.style';
 import MainBox from './components/common/MainBox';
 import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import {bgColor, colors, fontWeight} from './styles/theme'
+import { colors, fontWeight } from './styles/theme';
+import { Provider } from 'react-redux';
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
+import { productsApi } from './features/apiSlice';
+import { store } from './features/store';
 
 function App() {
   const theme = { ...colors, ...fontWeight };
 
-  console.log(theme)
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <MainBox>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='products' element={<Products />} />
-            <Route path='product/:id' element={<ProductDetail />} />
-            <Route path='shoppingcart' element={<ShoppingCart />} />
-          </Routes>
-        </MainBox>
-      </ThemeProvider>
+      <Provider store={store}>
+        <ApiProvider api={productsApi}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <MainBox>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='products' element={<Products />} />
+                <Route path='product/:id' element={<ProductDetail />} />
+                <Route path='shoppingcart' element={<ShoppingCart />} />
+              </Routes>
+            </MainBox>
+          </ThemeProvider>
+        </ApiProvider>
+      </Provider>
     </>
   );
 }
