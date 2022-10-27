b349aa8dd530bdde8b25c9a0de2688067566296b
Rhea Abayan <rhenayanz@gmail.com>
2022-10-27 21:46:20 +0800

    add apiProvider  #12 & install redux-persist   #19

--- src/features/store.js
+++ src/features/store.js
@@ -1,10 +1,35 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { productsApi } from "./apiSlice";
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { productsApi } from './apiSlice';
+import cartReducer from '../features/cartSlice';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const reducer = combineReducers({
+  cart: cartReducer,
+  [productsApi.reducerPath]: productsApi.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-    reducer: {
-        [productsApi.reducerPath]: productsApi.reducer,
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-      middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(productsApi.middleware),
-})
+    }).concat(productsApi.middleware),
+});
--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { productsApi } from './features/apiSlice';
+import { store } from './features/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const persistor = persistStore(store);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ApiProvider api={productsApi}>
+        <Provider store={store}>
+          <PersistGate persistor={persistor}>
+            <App />
+          </PersistGate>
+        </Provider>
+      </ApiProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
