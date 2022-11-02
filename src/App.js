d4f767ef9211732f8a3c6aa03378a84069c3e6d2
Rhea Abayan <rhenayanz@gmail.com>
2022-11-02 20:14:06 +0800

    config toast  #16, navbar #17, scrollToTop #21

--- src/App.js
+++ src/App.js
@@ -8,25 +8,30 @@ import GlobalStyle from './styles/GlobalStyles.style';
 import MainBox from './components/common/MainBox';
 import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import {ToastContainer} from 'react-toastify'
+import { ToastContainer } from 'react-toastify';
+import Navbar from './components/navbar';
+import ScrollToTop from './components/common/ScrollToTop';
 
 function App() {
   const theme = { ...colors, ...fontWeight };
 
   return (
-    <>   
-          <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <ToastContainer/>
-            <MainBox>
-              <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='products' element={<Products />} />
-                <Route path='product/:id' element={<ProductDetail />} />
-                <Route path='shoppingcart' element={<ShoppingCart />} />
-              </Routes>
-            </MainBox>
-          </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <ToastContainer />
+        <Navbar />
+        <MainBox>
+          <ScrollToTop>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='products' element={<Products />} />
+              <Route path='product/:id' element={<ProductDetail />} />
+              <Route path='shoppingcart' element={<ShoppingCart />} />
+            </Routes>
+          </ScrollToTop>
+        </MainBox>
+      </ThemeProvider>
     </>
   );
 }
